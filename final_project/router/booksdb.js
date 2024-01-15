c0dac947056124e3149bffec738df6265edea7ba
cagri32 <cagriahmet32@gmail.com>
2024-01-14 23:45:09 -0500

    Updated authenticated user routes

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,15 +1,14 @@
 const books = {
-    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {"user1": "Great book!", "user2": "Must-read!"}, "isbn": "isbn-1234567890"},
-    2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {"user1": "Magical stories!", "user2": "Classic tales!"}, "isbn": "isbn-2345678901"},
-    3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {"user1": "Epic masterpiece!", "user2": "Life-changing!"}, "isbn": "isbn-3456789012"},
-    4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {"user1": "Ancient treasure!", "user2": "Fascinating!"}, "isbn": "isbn-4567890123"},
-    5: {"author": "Unknown", "title": "The Book Of Job", "reviews": {"user1": "Profound wisdom!", "user2": "Deep insights!"}, "isbn": "isbn-5678901234"},
-    6: {"author": "Unknown", "title": "One Thousand and One Nights", "reviews": {"user1": "Arabian Nights delight!", "user2": "Entertaining!"}, "isbn": "isbn-6789012345"},
-    7: {"author": "Unknown", "title": "Nj\u00e1l's Saga", "reviews": {"user1": "Nordic saga!", "user2": "Historically rich!"}, "isbn": "isbn-7890123456"},
-    8: {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {"user1": "Classic romance!", "user2": "Timeless love!"}, "isbn": "isbn-8901234567"},
-    9: {"author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "reviews": {"user1": "French masterpiece!", "user2": "Compelling plot!"}, "isbn": "isbn-9012345678"},
-    10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"user1": "Existential journey!", "user2": "Philosophical depth!"}, "isbn": "isbn-0123456789"}
-  };
-  
-  module.exports = books;
-  
+    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": [{"username": "user1", "review": "Great book!"}, {"username": "user2", "review": "Must-read!"}], "isbn": "isbn-1234567890"},
+    2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": [{"username": "user1", "review": "Magical stories!"}, {"username": "user2", "review": "Classic tales!"}], "isbn": "isbn-2345678901"},
+    3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": [{"username": "user1", "review": "Epic masterpiece!"}, {"username": "user2", "review": "Life-changing!"}], "isbn": "isbn-3456789012"},
+    4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": [{"username": "user1", "review": "Ancient treasure!"}, {"username": "user2", "review": "Fascinating!"}], "isbn": "isbn-4567890123"},
+    5: {"author": "Unknown", "title": "The Book Of Job", "reviews": [{"username": "user1", "review": "Profound wisdom!"}, {"username": "user2", "review": "Deep insights!"}], "isbn": "isbn-5678901234"},
+    6: {"author": "Unknown", "title": "One Thousand and One Nights", "reviews": [{"username": "user1", "review": "Arabian Nights delight!"}, {"username": "user2", "review": "Entertaining!"}], "isbn": "isbn-6789012345"},
+    7: {"author": "Unknown", "title": "Nj\u00e1l's Saga", "reviews": [{"username": "user1", "review": "Nordic saga!"}, {"username": "user2", "review": "Historically rich!"}], "isbn": "isbn-7890123456"},
+    8: {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": [{"username": "user1", "review": "Classic romance!"}, {"username": "user2", "review": "Timeless love!"}], "isbn": "isbn-8901234567"},
+    9: {"author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "reviews": [{"username": "user1", "review": "French masterpiece!"}, {"username": "user2", "review": "Compelling plot!"}], "isbn": "isbn-9012345678"},
+    10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [{"username": "user1", "review": "Existential journey!"}, {"username": "user2", "review": "Philosophical depth!"}], "isbn": "isbn-0123456789"}
+};
+
+module.exports = books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,6 +35,11 @@ public_users.get('/', function (req, res) {
     res.send(JSON.stringify({ books }, null, 4));
 });
 
+// Get the user list available in the shop
+public_users.get('/users', function (req, res) {
+    res.send(JSON.stringify({ users }, null, 4));
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
