8fd2ac7c8794eb4ca8b9c968b1b4ecf94561b0c4
cagri32 <cagriahmet32@gmail.com>
2024-01-15 00:19:14 -0500

    Async axios calls needs to be improved

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,78 +1,91 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const doesExist = (username)=>{
-    let userswithsamename = users.filter((user)=>{
-      return user.username === username
+const doesExist = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username
     });
-    if(userswithsamename.length > 0){
-      return true;
+    if (userswithsamename.length > 0) {
+        return true;
     } else {
-      return false;
+        return false;
     }
-  }
-  
+}
+
 public_users.post("/register", (req, res) => {
     const username = req.query.username;
     const password = req.query.password;
-  
+
     if (username && password) {
-      if (!doesExist(username)) { 
-        users.push({"username":username,"password":password});
-        return res.status(200).json({message: "User successfully registred. Now you can login"});
-      } else {
-        return res.status(404).json({message: "User already exists!"});    
-      }
-    } 
-    return res.status(404).json({message: "Unable to register user. Username and/or password not provided"});
+        if (!doesExist(username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registred. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
+        }
+    }
+    return res.status(404).json({ message: "Unable to register user. Username and/or password not provided" });
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    res.send(JSON.stringify({ books }, null, 4));
+public_users.get('/', async function (req, res) {
+    try {
+        const response = await axios.get(`/`);
+        res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Get the user list available in the shop
-public_users.get('/users', function (req, res) {
-    res.send(JSON.stringify({ users }, null, 4));
+public_users.get('/users', async function (req, res) {
+    try {
+        const response = await axios.get('/users');
+        res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const book = Object.values(books).find(book => book.isbn === isbn);
-
-    if (book) {
-        res.send(JSON.stringify(book, null, 4));
-    } else {
-        res.send(`Book with ISBN ${isbn} not found.`);
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+        const response = await axios.get(`/isbn/${isbn}`);
+        res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-}); 
+});
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    const book = Object.values(books).find(book => book.author === author);
-
-    if (book) {
-        res.send(JSON.stringify(book, null, 4));
-    } else {
-        res.send(`Book with author name ${author} not found.`);
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const response = await axios.get(`/author/${author}`); // Replace with the actual API URL
+        res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    const book = Object.values(books).find(book => book.title === title);
-
-    if (book) {
-        res.send(JSON.stringify(book, null, 4));
-    } else {
-        res.send(`Book with title ${title} not found.`);
+public_users.get('/title/:title', async function (req, res) {
+    try {
+        const title = req.params.title;
+        const response = await axios.get(`/title/${title}`); // Replace with the actual API URL
+        res.send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
